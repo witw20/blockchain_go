2d2f17d24feaca49fba991d03199c99df85a0eb2
1v17 <166570730+1v17@users.noreply.github.com>
2025-05-30 20:20:43 -0700

    add ignore files and main from last stage

---
+++ task/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"time"
+)
+
+const (
+	blockchainSize = 5
+)
+
+type Block struct {
+	blockID      int
+	timestamp    int64
+	previousHash string
+}
+
+type Blockchain struct {
+	blocks []Block
+}
+
+func main() {
+	// declare a blockchain of 5 blocks in sequence
+	blocks := Blockchain{make([]Block, blockchainSize)}
+	for i := 0; i < blockchainSize; i++ {
+		if i == 0 {
+			blocks.blocks[i] = createBlock(i+1, time.Now().UnixNano(), "0")
+		} else {
+			blocks.blocks[i] = createBlock(i+1, time.Now().UnixNano(), calculateHash(blocks.blocks[i-1]))
+		}
+	}
+	printBlockchain(blocks)
+}
+
+func createBlock(blockID int, timestamp int64, prevBlockHash string) Block {
+	return Block{blockID, timestamp, prevBlockHash}
+}
+
+func calculateHash(block Block) string {
+	hash := sha256.New()
+	hash.Write([]byte(fmt.Sprintf("%d%d%s", block.blockID, block.timestamp, block.previousHash)))
+	return fmt.Sprintf("%x", hash.Sum(nil))
+}
+
+func validateBlock(block Block, prevBlock Block) bool {
+	if block.blockID != prevBlock.blockID+1 {
+		return false
+	}
+
+	if block.previousHash != calculateHash(prevBlock) {
+		return false
+	}
+
+	return true
+}
+
+func validateAllBlocks(blocks Blockchain) bool {
+	for i := 1; i < len(blocks.blocks); i++ {
+		if !validateBlock(blocks.blocks[i], blocks.blocks[i-1]) {
+			return false
+		}
+	}
+	return true
+}
+
+func printBlockchain(blocks Blockchain) {
+	for i, block := range blocks.blocks {
+		if i == 0 {
+			fmt.Printf("Genesis Block:\n")
+		} else {
+			fmt.Printf("Block:\n")
+		}
+		fmt.Printf("Id: %d\n", block.blockID)
+		fmt.Printf("Timestamp: %d\n", block.timestamp)
+		fmt.Printf("Hash of the previous block:\n%s\n", block.previousHash)
+		fmt.Printf("Hash of the block:\n%s\n", calculateHash(block))
+		fmt.Println()
+	}
+}
