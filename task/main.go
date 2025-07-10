0a62a4588f9a2ba0aabe468e27afbda8c7fb623d
1v17 <166570730+1v17@users.noreply.github.com>
2025-07-09 18:12:58 -0700

    refactor block and block chain

--- task/main.go
+++ task/main.go
@@ -10,75 +10,107 @@ import (
 
 const (
 	blockchainSize = 5
+	lowerTimeLimit = 5 * time.Second
+	upperTimeLimit = 10 * time.Second
+	minerNum       = 10
 )
 
 type Block struct {
 	blockID      int
+	creator      string
 	timestamp    int64
 	magicNumber  int32
 	previousHash string
+	thisHash     string
 	duration     time.Duration
 }
 
-func main() {
-	// declare a blockchain of 5 blocks in sequence
-	var zeros int
-	fmt.Print("Enter how many zeros the hash must start with:\n")
-	_, err := fmt.Scan(&zeros)
-	if err != nil {
-		fmt.Println("Error when scanning input: ", err)
-	}
-
-	zeroString := strings.Repeat("0", zeros)
-	blocks := make([]Block, blockchainSize)
-	for i := 0; i < blockchainSize; i++ {
-		if i == 0 {
-			blocks[i] = createBlock(i+1, time.Now().UnixNano(), zeroString, "0")
-		} else {
-			blocks[i] = createBlock(i+1, time.Now().UnixNano(), zeroString, calculateHash(blocks[i-1]))
-		}
-	}
-
-	printBlockchain(blocks)
+type BlockChain struct {
+	blocks     []Block
+	zeroString string
 }
 
-func createBlock(blockID int, timestamp int64, zeroString string, prevBlockHash string) Block {
+func main() {
+	blockChain := createBlockChain(blockchainSize, "")
+	for i := 0; i < blockchainSize; i++ {
+		if i == 0 {
+			blockChain.blocks[i] = *createBlock(i+1, time.Now().UnixNano(),
+				blockChain.zeroString, "0")
+		} else {
+			blockChain.blocks[i] = *createBlock(i+1, time.Now().UnixNano(),
+				blockChain.zeroString, blockChain.blocks[i-1].thisHash)
+		}
+		change := blockChain.adjustZero(blockChain.blocks[i].duration)
+		blockChain.blocks[i].printBlock()
+		printChange(change, blockChain.zeroString)
+	}
+}
+
+func createBlock(blockID int, timestamp int64, zeroString string, prevBlockHash string) *Block {
 	start := time.Now()
-	block := Block{blockID, timestamp, -1, prevBlockHash,
-		time.Duration(0)}
-	calculateMagicNumber(&block, zeroString)
+	block := &Block{blockID, "miner", timestamp, -1,
+		prevBlockHash, "", time.Duration(0)}
+	block.calculateMagicNumber(zeroString)
 	block.duration = time.Since(start)
 	return block
 }
 
-func calculateHash(block Block) string {
+func createBlockChain(length int, zeroString string) *BlockChain {
+	blockChain := BlockChain{make([]Block, length), zeroString}
+	return &blockChain
+}
+
+func (blockChain *BlockChain) adjustZero(duration time.Duration) int {
+	flag := 0
+	if duration < lowerTimeLimit {
+		blockChain.zeroString += "0"
+		flag = 1
+	} else if duration > upperTimeLimit {
+		blockChain.zeroString = strings.TrimSuffix(blockChain.zeroString, "0")
+		flag = -1
+	}
+	return flag
+}
+
+func (block *Block) calculateHash() string {
 	blockData := fmt.Sprintf("%d%d%d%s", block.blockID, block.timestamp, block.magicNumber, block.previousHash)
 	hash := sha256.New()
 	hash.Write([]byte(blockData))
 	return fmt.Sprintf("%x", hash.Sum(nil))
 }
 
-func calculateMagicNumber(block *Block, zeroString string) {
-	hushString := calculateHash(*block)
+func (block *Block) calculateMagicNumber(zeroString string) {
+	hushString := block.calculateHash()
 	for !strings.HasPrefix(hushString, zeroString) || block.magicNumber == -1 {
 		block.magicNumber = rand.Int31()
-		hushString = calculateHash(*block)
+		hushString = block.calculateHash()
 	}
+	block.thisHash = hushString
 }
 
-func printBlockchain(blocks []Block) {
-	for i, block := range blocks {
-		if i == 0 {
-			fmt.Printf("\nGenesis Block:\n")
-		} else {
-			fmt.Printf("Block:\n")
-		}
-		fmt.Printf("Id: %d\n", block.blockID)
-		fmt.Printf("Timestamp: %d\n", block.timestamp)
-		fmt.Printf("Magic number: %d\n", block.magicNumber)
-		fmt.Printf("Hash of the previous block:\n%s\n", block.previousHash)
-		fmt.Printf("Hash of the block:\n%s\n", calculateHash(block))
-		fmt.Printf("Block was generating for %.0f seconds\n", block.duration.Seconds())
-		fmt.Println()
+func (block *Block) printBlock() {
+
+	if block.blockID == 1 {
+		fmt.Printf("Genesis Block:\n")
+	} else {
+		fmt.Printf("\nBlock:\n")
+		fmt.Printf("Created by %s\n", block.creator)
+	}
+	fmt.Printf("Id: %d\n", block.blockID)
+	fmt.Printf("Timestamp: %d\n", block.timestamp)
+	fmt.Printf("Magic number: %d\n", block.magicNumber)
+	fmt.Printf("Hash of the previous block:\n%s\n", block.previousHash)
+	fmt.Printf("Hash of the block:\n%s\n", block.thisHash)
+	fmt.Printf("Block was generating for %.0f seconds\n", block.duration.Seconds())
+}
+
+func printChange(change int, zeros string) {
+	switch change {
+	case 1:
+		fmt.Printf("N was increased to %d\n", len(zeros))
+	case 0:
+		fmt.Print("N stays the same\n")
+	case -1:
+		fmt.Print("N was decreased by 1\n")
 	}
 }
